78a29ef8091884003e053d88e57f0a69aab28d06
Jessica Bealey <jfbealey@gmail.com>
2023-05-04 13:58:59 +0100

    More refactoring complete

--- directory.rb
+++ directory.rb
@@ -1,59 +1,63 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1.Print the menu and ask the user what to do
-    puts "1. Input the students "
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. Read the input and save it to a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit 
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = []
 
-      
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
-
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
+def print_menu
+  puts "1. Input the students "
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I dont't know what you mean, try again"
+  end
+end
+
+def show_students 
+  print_header
+  print
+  print_footer
+end
+      
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
